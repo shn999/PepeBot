bea5e0220b4fd689af2fd96736092fdee9dd4eb8
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-10-23 16:47:26 +0530

    Update balaji.py

--- stdplugins/balaji.py
+++ stdplugins/balaji.py
@@ -1,4 +1,4 @@
-"""COMMAND : .balaji"""
+"""COMMAND : .Mayur"""
 
 from telethon import events
 
@@ -22,7 +22,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "balaji":
+    if input_str == "Mayur":
 
         await event.edit(input_str)
 
@@ -30,7 +30,7 @@ async def _(event):
         
             "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @r4v4n4\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
-            "`Deleting Baalaji Repo....`",
+            "`Deleting Mayur Repo....`",
             "`Forking Uniborg... 0%\n\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 0 MiB / 108.7 MiB`",
             "`Forking Uniborg... 4%\n\n⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 22 MiB / 108.7 MiB`",
             "`Forking Uniborg... 8%\n\n⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 48 MiB / 108.7 MiB`",    
@@ -39,8 +39,8 @@ async def _(event):
             "`Forking Uniborg... 52%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 90.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 84%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️\n\nFile Size: 100.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 100%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️\n\nFile Size: 108.7 MiB / 108.7 MiB\n\nTask Completed... `",
-            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @kirito6969 ) \n`Verified Account:` ☑️\n`Official Website:` https://nehi hain koi website bhag bsdk\n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
-            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @kirito6969 ) \n`Verified Account:` ✅\n`Official Website:` https://nehi hain koi website bhag bsdk\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/prono69/PornHub"
+            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @Three_Cube_TeKnoways ) \n`Verified Account:` ☑️\n`Official Website:` https://nehi hain koi website \n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
+            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @Three_Cube_TeKnoways ) \n`Verified Account:` ✅\n`Official Website:` https://nehi hain koi website \n\n`Python` 3.6.8 (default, Dec 16 1981, 14:45:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/mkaraniya/PepeBot"
         ]
 
         for i in animation_ttl:
