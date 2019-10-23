dfe0ac4a60cc1bf5284901a72129565f957fd611
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-10-23 15:54:46 +0530

    Update PORNHUB.py

--- stdplugins/PORNHUB.py
+++ stdplugins/PORNHUB.py
@@ -166,3 +166,43 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 10])
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.5
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "hero":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "H_",
+
+            "HE_",
+
+            "HER_",
+
+            "HERO_",
+            
+            "HERO💪🏻_",
+            
+            "MAYUR KARANIYA💪🏻",
+            
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 10])
